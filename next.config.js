75e5b333a84e1ea9dec0aea7bf688ea5aae500a5
zyppayx-lab <zyppayx@gmail.com>
2026-06-14 21:49:21 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**"
-      }
-    ]
+    unoptimized: true
   }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
