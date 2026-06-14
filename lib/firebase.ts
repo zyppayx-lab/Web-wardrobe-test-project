0c3863b283a3ddc8a0fb313588f6ad5f1e764756
zyppayx-lab <zyppayx@gmail.com>
2026-06-14 18:42:55 +0100

    Create firebase.ts

---
+++ lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoSJggPoHfiI2quek_5m5GPSAhSaXFZE",
+  authDomain: "web-wardrobe.firebaseapp.com",
+  projectId: "web-wardrobe",
+  storageBucket: "web-wardrobe.appspot.com",
+  messagingSenderId: "201587857341",
+  appId: "1:201587857341:web:044fe04f2503168881a4d4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
