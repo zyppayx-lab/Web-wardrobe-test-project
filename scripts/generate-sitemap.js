0a40c1fa1546fd430216d46256aed70a4e0d1420
zyppayx-lab <zyppayx@gmail.com>
2026-06-18 10:30:45 +0100

    Create generate-sitemap.js

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+import { initBuild, safeSlug } from "./generate.js";
+
+const BASE_URL = "https://webwardrobe.name.ng";
+
+function buildUrl(post) {
+  const slug = safeSlug(post.slug || post.title);
+  return `${BASE_URL}/${post.category}/${slug}.html`;
+}
+
+async function run() {
+  const posts = await initBuild();
+
+  const urls = posts.map(p => `
+<url>
+  <loc>${buildUrl(p)}</loc>
+</url>`).join("");
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url><loc>${BASE_URL}</loc></url>
+${urls}
+</urlset>`;
+
+  fs.mkdirSync("dist", { recursive: true });
+  fs.writeFileSync("dist/sitemap.xml", sitemap);
+
+  console.log("Sitemap generated");
+}
+
+run();
