c0bf19a4bad11a6cc287432f87a209dccc4e8538
zyppayx-lab <zyppayx@gmail.com>
2026-06-18 10:21:39 +0100

    Create config.js

---
+++ firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBoSJggPoHfiI2quek_5m5G9PSAhSaXFZE",
+  authDomain: "web-wardrobe.firebaseapp.com",
+  projectId: "web-wardrobe",
+  storageBucket: "web-wardrobe.appspot.com",
+  messagingSenderId: "201587857341",
+  appId: "1:201587857341:web:044fe04f2503168881a4d4"
+};
